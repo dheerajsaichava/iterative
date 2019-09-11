71de7818aa4c92c918589232842d82766fd05fb3
dheerajsai chava <dheerajsaichava@gmail.com>
2019-09-10 21:14:20 -0400

    Add project files.

---
+++ iterative/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace iterative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("how many years of experience do u have in professional programming?");
+            string year = Console.ReadLine();
+                int years = int.Parse(year);
+            //Console.ReadKey(true);
+            
+                for (int i = 0; i <= years; i++)
+                {
+                    Console.WriteLine("you have" + i.ToString() + "years of experience ");
+                    
+
+            }
+            Console.ReadKey(true);
+        }
+        }
+}
